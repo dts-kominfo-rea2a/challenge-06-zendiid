3def8fdf74d65bf50c947854a9fa33b73ab2ee94
Zendi Cahya Y <zendicy@gmail.com>
2022-06-13 11:08:03 +0700

    Update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,32 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let arr1 = []
+  fs.readFile(file1, 'utf8', (err,data)=> {
+    if (err) {
+      return fnCallback(err,null)
+    } else {
+      arr1.push(JSON.parse(data).message.split(' ')[1])
+      fs.readFile(file2, 'utf8', (err, data2)=> {
+        if (err) {
+          return fnCallback(err,null)
+        } else {
+          arr1.push(JSON.parse(data2)[0].message.split(' ')[1])
+          fs.readFile(file3, 'utf8', (err, data3)=> {
+            if(err){
+              return fnCallback(err,null)
+            } else {
+              arr1.push(JSON.parse(data3)[0].data.message.split(' ')[1])
+              fnCallback(err, arr1)
+            }
+          })
+        }
+      })
+    }
+  })
+
+};;
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
